5ab642d33c5907b8d2d21b0c56a433bbff757d66
yiimnta <nguyentruongan.it.dlu@gmail.com>
2020-10-14 22:26:34 +0200

    add webapp-folder of exercise #1

---
+++ exercises/1/webapp/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <list-news v-bind:list="list" />
+  </div>
+</template>
+
+<script>
+import ListNews from './components/ListNews.vue';
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      list:[
+        {id:0, title:"Just", vote:0},
+        {id:1, title:"VueJS", vote:0},
+        {id:2, title:"Rocks", vote:0}
+      ]
+    }
+  },
+  components:{
+    ListNews
+  }
+}
+</script>
+<style>
+
+  #app {
+    font-family: 'Avenir', Arial, Helvetica, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color:#2c3e50;
+    width:500px;
+    margin:auto;
+    text-align: center;
+    max-height: 300px;
+  }
+
+  .list-item {
+    max-height: 600px;
+    overflow-y: auto;
+  }
+
+  .list-action-btn {
+    display: flex;
+    justify-content: center;
+    margin-top:10px;
+  }
+
+  .add-item  {
+    margin-top:50px;
+    display:flex;
+    justify-content: center;
+  }
+
+</style>
---
+++ exercises/1/webapp/src/components/News.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h2>{{news.title}} ({{news.vote}})</h2>
+        <div class="list-action-btn">
+            <button @click="updateItem(1)">Upvote</button>
+            <button @click="updateItem(-1)">Downvote</button>
+            <button @click="deleteItem">Remove</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props:{
+        news: Object,
+        default: null
+    },
+    methods: {
+        deleteItem : function(){
+            this.$emit("deleteNews", this.news);
+        },
+        updateItem : function(value){
+            this.news.vote += value;
+            this.$emit("sortNews");
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
