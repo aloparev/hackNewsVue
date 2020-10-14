c27ff3da471660898532b164ec1c656d27166c4b
yiimnta <nguyentruongan.it.dlu@gmail.com>
2020-10-14 22:26:34 +0200

    add webapp-folder of exercise #1

---
+++ exercises/1/webapp/src/components/CreateNews.vue
@@ -0,0 +1,26 @@
+<template>
+        <form onsubmit="return false;" class="add-item">
+            <input type="text" v-model="title" placeholder="title"/>
+            <button type="submit" v-on:click="addNews">Create</button>
+        </form>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            title:""
+        }
+    },
+    methods: {
+        addNews: function(){
+            this.$emit("addNews", {title:this.title, vote:0})
+            this.title = "";
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ exercises/1/webapp/src/components/ListNews.vue
@@ -0,0 +1,59 @@
+<template>
+    <div>
+        <div class="list-item">
+            <h1>News List</h1>
+            <news 
+                v-for="item in list"
+                v-bind:key="item.key"
+                v-bind:news="item" 
+                @sortNews="sortNews"
+                @deleteNews="deleteNews"
+            />
+        </div>
+        <create-news @addNews="addNews" />
+    </div>
+</template>
+ 
+<script>
+import News from "./News.vue"
+import CreateNews from './CreateNews.vue';
+
+export default {
+    name: 'list-news',
+    props:{
+        list: {
+            type: Array,
+            default: []
+        }
+    },
+    components: {
+        News,
+        CreateNews
+    },
+    methods:{
+        sortNews: function(){
+            this.list.sort((x,y) => y.vote - x.vote);
+        },
+        deleteNews: function(news){
+            this.list.splice(this.list.indexOf(news),1);
+        },
+        addNews: function(newNews){
+            let id = 0;
+            
+            if(this.list.length > 0) {
+                id = Math.max(...this.list.map(e => e.id));
+                id++;
+            }
+
+            newNews.id = id;
+            
+            this.list.push(newNews);
+            this.sortNews();
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
