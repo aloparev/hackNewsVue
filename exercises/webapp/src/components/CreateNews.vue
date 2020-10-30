e0147d05b020d31570dd283e8520a9c6dff98618
Ilona Eisenbraun <ilonaeisenbraun@gmail.com>
2020-10-30 09:24:42 +0100

    deleting unnecessary stuff

--- exercises/webapp/src/components/CreateNews.vue
+++ exercises/webapp/src/components/CreateNews.vue
@@ -1,31 +1,29 @@
 <template>
-        <form @submit.prevent="addNews" class="add-item">
-            <input type="text" v-model="title" placeholder="title"/>
-            <button type="submit" @click="addNews">Create</button>
-        </form>
+  <form @submit.prevent="addNews" class="add-item">
+    <input type="text" v-model="title" placeholder="title" />
+    <button type="submit" @click="addNews">Create</button>
+  </form>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            title:""
-        }
+  data() {
+    return {
+      title: "",
+    };
+  },
+  methods: {
+    addNews(e) {
+      if (!this.title || this.title.trim() == "") {
+        e.preventDefault();
+      }
+
+      this.$emit("add-news", { title: this.title, votes: 0 });
+      this.title = "";
     },
-    methods: {
-        addNews(){
-
-            if(!this.title || this.title.trim() == ""){
-                preventDefault();
-            }
-
-            this.$emit("addNews", {title:this.title, votes:0})
-            this.title = "";
-        }
-    }
-}
+  },
+};
 </script>
 
 <style>
-
 </style>
