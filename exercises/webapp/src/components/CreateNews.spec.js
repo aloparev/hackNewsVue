42d1e156533dced078919a82c82dd25a1ef6e68b
Ilona Eisenbraun <ilonaeisenbraun@gmail.com>
2020-10-30 18:32:05 +0100

    updating new structure and tests

--- exercises/webapp/src/components/CreateNews.spec.js
+++
@@ -1,8 +0,0 @@
-import CreateNews from './CreateNews.vue';
-
-describe('App', () => {
-    // Inspect the raw component options
-    it('has data', () => {
-      expect(typeof CreateNews.data).toBe('function')
-    })
-  });
