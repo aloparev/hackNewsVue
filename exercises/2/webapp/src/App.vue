b137c6f0a27369c9a6d16ccda688fcf6cf67df99
yiimnta <nguyentruongan.it.dlu@gmail.com>
2020-10-30 07:42:59 +0100

    fix bug and upload the folder webapp into excercise 2
    content fix bug :
    - CreateNews.vue
    + preventDefault() => return
    
    - ListNews.vue
    + create a new method "update()"
    + rename "getSortedNews" to "sortedNews" at 6th-line

---
+++ exercises/2/webapp/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <list-news/>
+  </div>
+</template>
+
+<script>
+import ListNews from './components/ListNews.vue';
+
+export default {
+  name: 'app',
+  components:{
+    ListNews
+  }
+}
+</script>
+<style>
+
+  #app {
+    font-family: 'Avenir', Arial, Helvetica, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color:#2c3e50;
+    width:500px;
+    margin:auto;
+    text-align: center;
+    max-height: 300px;
+  }
+
+  .list-item {
+    max-height: 600px;
+    overflow-y: auto;
+  }
+
+  .list-action-btn {
+    display: flex;
+    justify-content: center;
+    margin-top:10px;
+  }
+
+  .add-item  {
+    margin-top:50px;
+    display:flex;
+    justify-content: center;
+  }
+
+</style>
