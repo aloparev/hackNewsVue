ef933b06994129c8c195e2ed54a26f16d6fc6498
yiimnta <nguyentruongan.it.dlu@gmail.com>
2020-10-30 10:11:53 +0100

    fix bug

--- exercises/webapp/src/components/CreateNews.vue
+++ exercises/webapp/src/components/CreateNews.vue
@@ -1,41 +1,27 @@
 <template>
-  <form
-    class="add-item"
-    @submit.prevent="addNews"
-  >
-    <input
-      v-model="title"
-      type="text"
-      placeholder="title"
-    >
-    <button
-      type="submit"
-      @click="addNews"
-    >
-      Create
-    </button>
-  </form>
+        <form @submit.prevent="addNews" class="add-item">
+            <input type="text" v-model="title" placeholder="title"/>
+            <button type="submit" @click="addNews">Create</button>
+        </form>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      title: "",
-    };
-  },
-  methods: {
-    addNews(e) {
-      if (!this.title || this.title.trim() == "") {
-        e.preventDefault();
-      }
-
-      this.$emit("add-news", { title: this.title, votes: 0 });
-      this.title = "";
+    data() {
+        return {
+            title:''
+        };
     },
-  },
+    methods: {
+        addNews(){
+
+            if(!this.title || this.title.trim() == ''){
+                return;
+            }
+
+            this.$emit('addNews', {title:this.title, votes:0});
+            this.title = '';
+        }
+    }
 };
 </script>
-
-<style>
-</style>
