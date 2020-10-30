17b0234319679ed018e3bc0c37f6ca67a81161f2
yiimnta <nguyentruongan.it.dlu@gmail.com>
2020-10-30 10:11:53 +0100

    fix bug

--- exercises/webapp/src/components/News.vue
+++ exercises/webapp/src/components/News.vue
@@ -1,38 +1,29 @@
 <template>
-  <div>
-    <h2>{{ news.title }} ({{ news.votes }})</h2>
-    <div class="list-action-btn">
-      <button @click="updateItem(1)">
-        Upvote
-      </button>
-      <button @click="updateItem(-1)">
-        Downvote
-      </button>
-      <button @click="deleteItem">
-        Remove
-      </button>
+    <div>
+        <h2>{{news.title}} ({{news.votes}})</h2>
+        <div class="list-action-btn">
+            <button @click="updateItem(1)">Upvote</button>
+            <button @click="updateItem(-1)">Downvote</button>
+            <button @click="deleteItem">Remove</button>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
 export default {
-  props: {
-    news: {
-      type: Object,
-      default: () => ({}),
+    props:{
+        news: {
+            type: Object,
+            default: () => ({})
+        }
     },
-  },
-  methods: {
-    deleteItem: function () {
-      this.$emit("delete-news");
-    },
-    updateItem: function (value) {
-      this.$emit("update", { ...this.news, votes: this.news.votes + value });
-    },
-  },
+    methods: {
+        deleteItem : function(){
+            this.$emit('deleteNews');
+        },
+        updateItem : function(value){
+            this.$emit('update', {...this.news, votes: (this.news.votes + value)});
+        }
+    }
 };
 </script>
-
-<style>
-</style>
