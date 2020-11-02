a2d37f81306243d74cd93f45157ed7f9f258f444
Ilona Eisenbraun <ilonaeisenbraun@gmail.com>
2020-11-02 18:05:01 +0100

    updating stories to our components

---
+++ exercises/webapp/.storybook/main.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "stories": [
+    "../src/components/**/*.stories.js",
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-controls",
+    "@storybook/addon-a11y"
+  ]
+}
