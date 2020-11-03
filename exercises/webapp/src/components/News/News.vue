cadac42a7e89d0dc80f7166cbff7726895cce550
yiimnta <nguyentruongan.it.dlu@gmail.com>
2020-11-03 02:24:21 +0100

    Add Storybook of component News and FIX name of News
    Add Storybook of component News
    +Add aria-label for some buttons
    
    FIX name of News (News.vue)

--- exercises/webapp/src/components/News/News.vue
+++ exercises/webapp/src/components/News/News.vue
@@ -2,16 +2,16 @@
   <div>
     <h2>{{ news.title }} ({{ news.votes }})</h2>
     <div class="list-action-btn">
-      <button @click="updateItem(1)">Upvote</button>
-      <button @click="updateItem(-1)">Downvote</button>
-      <button @click="deleteItem">Remove</button>
+      <button aria-label="Upvote" @click="updateItem(1)">Upvote</button>
+      <button aria-label="Downvote" @click="updateItem(-1)">Downvote</button>
+      <button aria-label="Remove" @click="deleteItem">Remove</button>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "ListNews",
+  name: "News",
   props: {
     news: {
       type: Object,
