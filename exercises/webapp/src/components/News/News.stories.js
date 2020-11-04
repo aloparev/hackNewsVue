2ba09895cdcbfd0927e7fa4ec7e8119a40c80eac
Ilona <ilonaeisenbraun@gmail.com>
2020-11-04 10:06:08 +0100

    quick fix

--- exercises/webapp/src/components/News/News.stories.js
+++ exercises/webapp/src/components/News/News.stories.js
@@ -3,21 +3,22 @@ import News from "./News.vue";
 
 export default {
   title: "News",
-  component: News
+  component: News,
 };
 
 const Template = (args, { argTypes }) => ({
   props: Object.keys(argTypes),
-  components: { News},
-  template: '<News @update="updateNews" @delete-news="deleteNews" v-bind="$props" />',
-  methods: { updateNews: action('clicked'), deleteNews: action('clicked') }
+  components: { News },
+  template:
+    '<News @update="updateItem" @delete-news="deleteItem" v-bind="$props" />',
+  methods: { updateItem: action("clicked"), deleteItem: action("clicked") },
 });
 
 export const NewsItem = Template.bind({});
 NewsItem.args = {
-    news:{
-      id: 0,
-      title: 'Example title',
-      votes: 3
-    }
+  news: {
+    id: 0,
+    title: "Example",
+    votes: 3,
+  },
 };
