be35dc7e470a34b63a21180b3a5e69269715c12b
Ilona <ilonaeisenbraun@gmail.com>
2020-11-04 10:06:08 +0100

    quick fix

--- exercises/webapp/src/components/CreatNews/CreateNews.stories.js
+++ exercises/webapp/src/components/CreatNews/CreateNews.stories.js
@@ -1,24 +1,18 @@
 import CreateNews from "./CreateNews.vue";
-import { action } from '@storybook/addon-actions';
-import { storiesOf } from '@storybook/vue';
+import { action } from "@storybook/addon-actions";
 
 export default {
   title: "CreateNews",
   component: CreateNews,
-  argTypes: {
-    backgroundColor: { control: "color" }
-  },
+  argTypes: {},
 };
 
 const Template = (args, { argTypes }) => ({
   props: Object.keys(argTypes),
   components: { CreateNews },
   template: '<CreateNews  @add-news="add" v-bind="$props"/>',
-  methods: { add: action('clicked') }
+  methods: { add: action("clicked") },
 });
 
-
-export const Red = Template.bind({});
-Red.args = {
-  backgroundColor: "#e00",
-};
+export const NewsForm = Template.bind({});
+NewsForm.args = {};
