c170992ffa8de7f9f995ce8caa07b77290a2ae00
yiimnta <nguyentruongan.it.dlu@gmail.com>
2020-11-07 00:52:07 +0100

    Edit CSS, Add error message when create failed
    - Edit Css: App.vue,  CreateNews, ListNews
    - Rename some class
    - Add error message when create failed (CreateNews)

--- exercises/webapp/src/components/CreatNews/CreateNews.vue
+++ exercises/webapp/src/components/CreatNews/CreateNews.vue
@@ -9,6 +9,7 @@
     <button type="submit" :disabled="!title.length > 0" @click="addNews">
       Create
     </button>
+    <p class="error-text">{{ error_message }}</p>
   </form>
 </template>
 
@@ -18,17 +19,26 @@ export default {
   data() {
     return {
       title: "",
+      error_message: "",
     };
   },
   methods: {
     addNews() {
       if (!this.title || this.title.trim() == "") {
-        return;
+        this.error_message = "Create failed! Title cannot be empty!";
+      } else {
+        this.$emit("add-news", { title: this.title, votes: 0 });
+        this.error_message = "";
       }
 
-      this.$emit("add-news", { title: this.title, votes: 0 });
       this.title = "";
     },
   },
 };
 </script>
+
+<style scoped>
+.error-text {
+  color: red;
+}
+</style>
