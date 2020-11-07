182b62bd609689a2b7ace051a9f820fe5c53a3c6
Ilona <ilonaeisenbraun@gmail.com>
2020-11-07 09:47:11 +0100

    FIX: working through reviews

--- exercises/webapp/.storybook/webpack.config.js
+++ exercises/webapp/.storybook/webpack.config.js
@@ -1,10 +1,6 @@
 const path = require('path');
 
 module.exports = ({ config }) => {
-    /*  config.resolve.modules = [
-        path.resolve(__dirname, ".."),
-        "node_modules",
-      ]*/
     config.resolve.alias = {
         ...config.resolve.alias,
         "@": path.resolve(__dirname, "../src")
