cdb6924a6854a319f36f048ba5061379442dd70e
yiimnta <nguyentruongan.it.dlu@gmail.com>
2020-11-19 06:34:27 +0100

    Fix bug, adding mutation in NewsList and in News
    backend:
    - fix downvotePost ()
    
    webapp:
    -  News : adding mutation upvote(), downvote()
    -  ListNews : get postlist, repair the function addNews, fix temporary duplicate id error when creating.
    -  CreateNews: add new variable currentUser, return id of new post when using emit.
    
    Note: Some bugs occur when deleting and creating

--- webapp/src/components/News/News.vue
+++ webapp/src/components/News/News.vue
@@ -2,8 +2,8 @@
   <div>
     <h2>{{ news.title }} ({{ news.votes }})</h2>
     <div>
-      <button class="upvote" @click="updateItem(1)">Upvote</button>
-      <button class="downvote" @click="updateItem(-1)">Downvote</button>
+      <button class="upvote" @click="upvote">Upvote</button>
+      <button class="downvote" @click="downvote">Downvote</button>
       <button class="delete" @click="deleteItem">Remove</button>
     </div>
   </div>
@@ -20,29 +20,96 @@ export default {
       required: true,
     },
   },
+  data() {
+    return {
+      currentUser: { name: "TestUser" },
+    };
+  },
   methods: {
     async deleteItem() {
       try {
-        await this.$apollo.mutate({
-          mutation: gql`
-            mutation($id: ID!) {
-              delete(id: $id) {
-                title
+        const response = await this.$apollo
+          .mutate({
+            mutation: gql`
+              mutation($id: ID!) {
+                delete(id: $id) {
+                  title
+                }
               }
-            }
-          `,
-          variables: {
-            id: this.news.id,
-          },
-        });
+            `,
+            variables: {
+              id: this.news.id,
+            },
+          })
+          .catch(console.error);
 
+        console.log(response.data.delete);
         this.$emit("delete-news");
       } catch {
         throw new Error("Mutation 'delete' failed!");
       }
     },
-    updateItem(value) {
-      this.$emit("update", { ...this.news, votes: this.news.votes + value });
+    async upvote() {
+      try {
+        const response = await this.$apollo
+          .mutate({
+            mutation: gql`
+              mutation($id: ID!, $voter: UserInput!) {
+                upvote(id: $id, voter: $voter) {
+                  title
+                  votes
+                  voters {
+                    name
+                  }
+                }
+              }
+            `,
+            variables: {
+              id: this.news.id,
+              voter: this.currentUser,
+            },
+          })
+          .catch(console.error);
+
+        console.log(response.data.upvote);
+        this.$emit("update", {
+          ...this.news,
+          votes: response.data.upvote.votes,
+        });
+      } catch {
+        throw new Error("Mutation 'upvote' failed!");
+      }
+    },
+    async downvote() {
+      try {
+        const response = await this.$apollo
+          .mutate({
+            mutation: gql`
+              mutation($id: ID!, $voter: UserInput!) {
+                downvote(id: $id, voter: $voter) {
+                  title
+                  votes
+                  voters {
+                    name
+                  }
+                }
+              }
+            `,
+            variables: {
+              id: this.news.id,
+              voter: this.currentUser,
+            },
+          })
+          .catch(console.error);
+
+        console.log(response.data.downvote);
+        this.$emit("update", {
+          ...this.news,
+          votes: response.data.downvote.votes,
+        });
+      } catch {
+        throw new Error("Mutation 'downvote' failed!");
+      }
     },
   },
 };
