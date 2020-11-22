50d9bb98db3875d9b24ab84eabcd5d9f950160c8
yiimnta <nguyentruongan.it.dlu@gmail.com>
2020-11-22 03:44:26 +0100

    fixing bug, add test files
    - Add nodemon, crypto
    - Add Test files
    - Fix bug duplicate ID when creating
    - Add classes User and Post
    - Edit server.js, post- and users-data-source.js
    
    Co-Authored-By: Andrej <11981882+aloparev@users.noreply.github.com>

--- backend/src/resolver.js
+++ backend/src/resolver.js
@@ -1,32 +1,32 @@
 const resolvers = {
     Query: {
-      posts: async (parent, args, {dataSources}) => {
-        return await dataSources.postsDataSrc.allPosts()
+      posts: async (parent, args, context) => {
+        return await context.dataSources.postsDataSrc.allPosts()
       },
-      users: async (parent, args, {dataSources}) => {
-        return await dataSources.usersDataSrc.allUsers()
+      users: async (parent, args, context) => {
+        return await context.dataSources.usersDataSrc.allUsers()
       }
     },
     User: {
-      posts: async (parent, args, {dataSources}) => {
-        let posts = await dataSources.postsDataSrc.allPosts();
+      posts: async (parent, args, context) => {
+        let posts = await context.dataSources.postsDataSrc.allPosts();
         return posts.filter(e => e.author.name == parent.name)
       }
     },
     Mutation: {
-      write: async (parent, args, {dataSources}) => {
+      write: async (parent, args, context) => {
         console.log('Mutation.write.postInput', args.post)
-        return await dataSources.postsDataSrc.createPost(args.post)
+        return await context.dataSources.postsDataSrc.createPost(args.post)
       },
-      upvote: async (parent, args, {dataSources}) => {
+      upvote: async (parent, args, context) => {
         console.log('Mutation.upvote.args', args)
-        return await dataSources.postsDataSrc.upvotePost(args.id, args.voter)
+        return await context.dataSources.postsDataSrc.upvotePost(args.id, args.voter)
       },
-      downvote: async (parent, args, {dataSources}) => {
-        return await dataSources.postsDataSrc.downvotePost(args.id, args.voter)
+      downvote: async (parent, args, context) => {
+        return await context.dataSources.postsDataSrc.downvotePost(args.id, args.voter)
       },
-      delete: async (parent, args, {dataSources}) => {
-        return await dataSources.postsDataSrc.deletePost(args.id)
+      delete: async (parent, args, context) => {
+        return await context.dataSources.postsDataSrc.deletePost(args.id)
       }
     }
 };
