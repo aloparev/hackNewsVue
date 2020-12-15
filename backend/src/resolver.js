09380fc4fda6852aeda587af2f94e362f9ffa63b
yiimnta <nguyentruongan.it.dlu@gmail.com>
2020-12-15 21:03:56 +0100

    connecting with Graphcms
    Co-Authored-By: Andrej <11981882+aloparev@users.noreply.github.com>
    
    	modified:   backend/package.json
    	new file:   backend/src/config.js
    	modified:   backend/src/context.js
    	new file:   backend/src/graphCms/executor.js
    	new file:   backend/src/graphCms/schema.js
    	modified:   backend/src/index.js
    	modified:   backend/src/permissions.js
    	modified:   backend/src/resolver.js
    	modified:   backend/src/schema.js
    	modified:   backend/src/server.js
    	deleted:    backend/src/token.js
    	new file:   backend/src/typeDefs.js
    	modified:   yarn.lock

--- backend/src/resolver.js
+++ backend/src/resolver.js
@@ -1,43 +1,109 @@
-const resolvers = {
+const { delegateToSchema } = require('@graphql-tools/delegate');
+const executor = require('./graphCms/executor');
+const bcrypt = require('bcrypt');
+const {UserInputError} = require('apollo-server');
+
+module.exports = ({ subschema }) => ({
   Query: {
-    posts: async (parent, args, context) => {
-      return await context.dataSources.postsDataSrc.allPosts()
-    },
-    users: async (parent, args, context) => {
-      return await context.dataSources.usersDataSrc.allUsers()
+  },
+  Person:{
+    postCount: {
+      selectionSet: '{ posts {id} }',
+      resolve: (person) =>  person.posts.length
     }
   },
-  User: {
-    posts: async (parent, args, context) => {
-      const posts = await context.dataSources.postsDataSrc.allPosts()
-      return posts.filter(e => e.author.id === parent.id)
-    }
-  },
-  Post: {
-    votes: async (parent) => {
-      return parent.getVotes()
+  Post:{
+    authored:{
+      selectionSet: '{ author {id} }',
+      resolve: async (post, args, context, info) =>  {
+        if(context.person){
+          return context.person.id === post.author.id;
+        }
+
+        return false;
+      }
     }
   },
   Mutation: {
     write: async (parent, args, context) => {
-      return await context.dataSources.postsDataSrc.createPost(args.post)
+      //TODO
+      return null;
     },
     upvote: async (parent, args, context) => {
-      return await context.dataSources.postsDataSrc.votePost(args.id, 1)
+      //TODO
+      return null;
     },
     downvote: async (parent, args, context) => {
-      return await context.dataSources.postsDataSrc.votePost(args.id, -1)
+      //TODO
+      return null;
     },
     delete: async (parent, args, context) => {
-      return await context.dataSources.postsDataSrc.deletePost(args.id)
+      //TODO
+      return null;
     },
     signup: async (parent, args, context) => {
-      return await context.dataSources.usersDataSrc.signup(args.name, args.email, args.password)
+
+      const { name, email, password } = args;
+
+      const checkEmailquery = `{
+        person(where:{email:"${email}"}){
+          id
+        }
+      }`;
+      const {data:{person}} = await executor(checkEmailquery);
+      console.log("Person has same email:", person)
+
+      if(!person) {
+          
+          const createQuery = `
+            mutation{
+              createPerson(data: {name:"${name}", email:"${email}",password:"${bcrypt.hashSync(password, 10)}"}){
+                id
+              }
+            }
+          `;
+
+          const {data:{createPerson}} = await executor(createQuery)
+          console.log("Created Person: ",createPerson)
+
+          // //publish Person
+          // const publishQuery = `
+          //   mutation{
+          //     publishPerson(where:{id:"${createPerson.id}"}, to: PUBLISHED){
+          //       id
+          //     }
+          //   }
+          // `;
+
+          // const {data: {publishPerson}} = await executor(publishQuery)
+          // console.log("Published Person",publishPerson)
+          
+          if(createPerson) {
+            return context.jwtSign({ person: { id: createPerson.id } })
+          }
+
+      }else {
+        return new UserInputError("Email already exists")
+      }
+
+      return null;
+      
     },
-    login: async (parent, args, context) => {
-      return await context.dataSources.usersDataSrc.login(args.email)
+    login: async (parent, args, context, info) => {
+      const { email, password } = args;
+      const query = `{
+          person(where:{email:"${email}"}){
+            id
+            password
+          }
+      }`;
+      const {data:{person}} = await executor(query)
+      
+      if(bcrypt.compareSync(password, person.password)) {
+        return context.jwtSign({ person: { id: person.id } })
+      }
+
+      return null;
     }
   }
-}
-
-module.exports = resolvers
+});
---
+++ backend/src/typeDefs.js
@@ -0,0 +1,27 @@
+const { gql } = require('apollo-server')
+
+const typeDefs = gql`
+
+  type Mutation {
+    write(post: PostInput!): Post
+    upvote(id: ID!): Post
+    downvote(id: ID!): Post
+    delete(id: ID!): Post
+    login(email: String!, password: String!): String
+    signup(name: String!, email: String!, password: String!): String
+  }
+
+  extend type Person {
+    postCount: Int
+  }
+  
+  input PostInput {
+    title: String!
+  }
+
+  extend type Post {
+    authored: Boolean
+  }
+`
+
+module.exports = typeDefs
