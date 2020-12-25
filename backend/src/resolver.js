e1ce35cae0c8e5831a993fcc6847635744250e27
yiimnta <nguyentruongan.it.dlu@gmail.com>
2020-12-25 18:11:52 +0100

    add delete Post

--- backend/src/resolver.js
+++ backend/src/resolver.js
@@ -1,6 +1,5 @@
-const bcrypt = require('bcrypt');
-const {UserInputError, AuthenticationError, gql} = require('apollo-server');
-const {writePost, checkEmailExist, votePost} = require('./utils')
+
+const {writePost, votePost, deletePost, signup, login} = require('./utils')
 
 module.exports = ([{ schema, executor }]) => ({
   Query: {
@@ -40,52 +39,17 @@ module.exports = ([{ schema, executor }]) => ({
 
       return await votePost(context.person.id, args.id, -1 , schema, executor, context, info);
     },
-    delete: async (_, args, context) => {
-      //TODO
-      return null;
+    delete: async (_, args, context, info) => {
+
+      return await deletePost(context.person.id, args.id, schema, executor, context, info);
     },
     signup: async (_, args, context) => {
-      const { name, email, password } = args;
 
-      if (await checkEmailExist(email, executor)) {
-        throw new UserInputError("Email already exist");
-      }
-
-      //insert the new user in database
-      let document = gql`
-      mutation ($name: String!, $email: String!, $password: String!) {
-        createPerson(data: {name: $name, email: $email, password: $password}) {
-          id
-        }
-      }
-      `;
-
-      const passwordHash = bcrypt.hashSync(password, 10);
-      let response = await executor({ document, variables : { name, email, password: passwordHash} });
-
-      if (response.errors) throw new UserInputError(response.errors.map((e) => e.message).join('\n'));
-
-      return context.jwtSign({ person: { id: response.data.createPerson.id } });
+      return await signup(args, executor, context);
     },
-    login: async (parent, args, context, info) => {
+    login: async (_, args, context) => {
       
-      const document  = gql`
-        query ($email: String!) {
-          person(where:{email:$email}){
-            id
-            password
-          }
-        }`;
-
-      const { email, password } = args;
-      const { data, errors } = await executor({ document, variables: { email, password } });
-      if (errors) throw new UserInputError(errors.map((e) => e.message).join('\n'));
-      const { person } = data;
-      if(person && bcrypt.compareSync(password, person.password)) {
-          return context.jwtSign({ person: { id: person.id } })
-      }
-
-      throw new AuthenticationError('Wrong email/password combination');
+      return await login(args, executor, context);
     }
   }
 });
--- backend/src/utils.js
+++ backend/src/utils.js
@@ -1,5 +1,57 @@
 const {UserInputError, AuthenticationError, gql} = require('apollo-server');
 const { delegateToSchema } = require('@graphql-tools/delegate');
+const bcrypt = require('bcrypt');
+
+const login = async(args, executor, context) => {
+  const document  = gql`
+    query ($email: String!) {
+      person(where:{email:$email}){
+        id
+        password
+      }
+    }`;
+
+  const { email, password } = args;
+  const { data, errors } = await executor({ document, variables: { email, password } });
+  
+  if (errors) {
+    throw new UserInputError(errors.map((e) => e.message).join('\n'));
+  }
+
+  const { person } = data;
+
+  if(person && bcrypt.compareSync(password, person.password)) {
+      return context.jwtSign({ person: { id: person.id } })
+  }
+
+  throw new AuthenticationError('Wrong email/password combination');
+}
+
+const signup = async(args, executor, context) => {
+  const { name, email, password } = args;
+
+  if (await checkEmailExist(email, executor)) {
+    throw new UserInputError("Email already exist");
+  }
+
+  //insert the new user in database
+  let document = gql`
+  mutation ($name: String!, $email: String!, $password: String!) {
+    createPerson(data: {name: $name, email: $email, password: $password}) {
+      id
+    }
+  }
+  `;
+
+  const passwordHash = bcrypt.hashSync(password, 10);
+  let response = await executor({ document, variables : { name, email, password: passwordHash} });
+
+  if (response.errors) {
+    throw new UserInputError(response.errors.map((e) => e.message).join('\n'));
+  }
+
+  return context.jwtSign({ person: { id: response.data.createPerson.id } });
+}
 
 //check user exist
 const checkUserExist = async (userId, executor) => {
@@ -18,7 +70,6 @@ const checkUserExist = async (userId, executor) => {
   return !!response.data.person;
 }
 
-//check post exist
 const checkPostExist = async (postId, executor) => {
   let document  = gql`
   query ($id: ID!) {
@@ -35,7 +86,6 @@ const checkPostExist = async (postId, executor) => {
   return !!response.data.post;
 }
 
-//check post exist
 const checkEmailExist = async (email, executor) => {
   let document  = gql`
   query ($email: String!) {
@@ -57,30 +107,21 @@ const mayVote = async(userId, postId, executor) => {
   
   //get the list voters of User
   const document  = gql`
-  query ($id: ID!) {
-    person(where:{id:$id}){
-      voters{
-        post {
-          id
-        }
-      }
+  query ($userId: ID!, $postId: ID!) {
+    voters(where: {person: {id: $userId}, post: {id: $postId}}) {
+      id
     }
   }`;
 
-  const { data, errors } = await executor({ document, variables: { id: userId} });
+  const { data, errors } = await executor({ document, variables: { userId: userId, postId: postId} });
   
   if (errors) {
     throw new UserInputError(response.errors.map((e) => e.message).join('\n'));
   }
   
-  const { person } = data;
+  const { voters } = data;
 
-  if(person){
-    const voter = person.voters.find(e => e.post.id === postId)
-    return voter == null;
-  }
-
-  return false;
+  return voters != null && voters.length == 0;
 }
 
 //val = 1 or -1
@@ -172,4 +213,63 @@ const writePost = async(userId, args, schema, executor, context, info) => {
   return createPost;
 }
 
-module.exports = { checkEmailExist , votePost, writePost }
+const mayDelete = async(userId, postId, executor) => {
+  
+  //get the Post with user id and post id to check author
+  const document  = gql`
+  query ($userId: ID!, $postId: ID!) {
+    posts(where: {author: {id: $userId}, id: $postId}) {
+      id
+    }
+  }`;
+
+  const { data, errors } = await executor({ document, variables: { userId: userId, postId: postId } });
+  
+  if (errors) {
+    throw new UserInputError(response.errors.map((e) => e.message).join('\n'));
+  }
+  
+  const { posts } = data;
+
+  return posts != null && posts.length > 0; // length == 1
+}
+
+const deletePost = async(userId, postId, schema, executor, context, info) => {
+  if(!await checkUserExist(userId, executor)) { //user is not exist
+    throw new AuthenticationError("Sorry, your credentials are wrong!");
+  }
+
+  if(!await mayDelete(userId, postId, executor)) { //check author
+    throw new UserInputError("Only the author of the post may delete the post.");
+  }
+
+  //delete the list Voters of the Post
+  const document  = gql`
+  mutation ($postId: ID!) {
+    deleteManyVoters(where: {post: {id: $postId}}) {
+      count
+    }
+  }`;
+
+  const {errors} = await executor({ document, variables: { userId: userId, postId: postId} });
+  
+  if (errors) {
+    throw new UserInputError(errors.map((e) => e.message).join('\n'));
+  }
+
+  const deletedPost = await delegateToSchema({
+    schema,
+    operation: 'mutation',
+    fieldName: 'deletePost',
+    args: {
+      where:{id: postId}
+    },
+    context,
+    info
+  });
+
+  //how can we do rollback (restore the deleted voters)? if we meet errors in deleting the post
+  return deletedPost;
+}
+
+module.exports = {votePost, writePost, deletePost, signup, login}
